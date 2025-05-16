9ea886bb4d40ddfd577d8ab4d858dacbd3681cd1
Kasyanov-Igor <igor.kasyanov00@mail.ru>
2025-05-16 20:43:33 +0300

    Added post requests for registration and login. Closed #8

--- Pages/Clients.cshtml.cs
+++ Pages/Clients.cshtml.cs
@@ -1,3 +1,4 @@
+using gym_project_business_logic.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,14 @@ namespace GymProjectClient.Pages
 {
 	public class ClientsModel : PageModel
 	{
+		public Client client { get; set; }
 		public void OnGet()
 		{
+		}
+
+		public void OnPostLogin([FromBody] Client clientLogin) 
+		{ 
+			this.client = clientLogin;
 		}
 	}
 }
