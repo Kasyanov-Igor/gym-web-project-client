97d9b6fc0a94511a5409b47b780062f832ea38b0
Igor <161635179+Kasyanov-Igor@users.noreply.github.com>
2025-05-22 10:45:47 +0300

    Added transition on login to another page. Closed #19

---
+++ Pages/CoachWorkData.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GymProjectClient.Pages
+{
+    public class CoachWorkDataModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
